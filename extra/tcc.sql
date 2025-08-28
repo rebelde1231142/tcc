22bfa81c08870cf02d489e7c65b99ad72d0d84d6
Leonel Breno da Silva Ramos de Paula <leonelbrenodasilvagithub@gmail.com>
2025-08-28 20:00:28 -0300

    voltou a funcionar
    resolvi alguns dos problemas

--- extra/tcc.sql
+++ extra/tcc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 27-Ago-2025 às 15:14
--- Versão do servidor: 8.0.30
--- versão do PHP: 8.3.4
+-- Generation Time: Aug 28, 2025 at 10:57 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `tcc`
+-- Database: `tcc`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categoria`
+-- Table structure for table `categoria`
 --
 
 CREATE TABLE `categoria` (
@@ -34,7 +34,7 @@ CREATE TABLE `categoria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `categoria`
+-- Dumping data for table `categoria`
 --
 
 INSERT INTO `categoria` (`Id`, `Nome`, `Descricao`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `categoria` (`Id`, `Nome`, `Descricao`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `entrada`
+-- Table structure for table `entrada`
 --
 
 CREATE TABLE `entrada` (
@@ -58,31 +58,36 @@ CREATE TABLE `entrada` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `itens`
+-- Table structure for table `itens`
 --
 
 CREATE TABLE `itens` (
   `id` int NOT NULL,
   `nome` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `quantidade` int DEFAULT NULL,
-  `tipo_quantidade` varchar(20) DEFAULT 'unidade',
   `descricao` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `fk_Categoria_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `itens`
+-- Dumping data for table `itens`
 --
 
-INSERT INTO `itens` (`id`, `nome`, `quantidade`, `tipo_quantidade`, `descricao`, `fk_Categoria_id`) VALUES
-(36, 'béquer', 100, 'unidade', 'asasssasaas', 3),
-(53, 'hdd', 20, 'unidade', 'armazenamento', 1),
-(54, 'hdd', 20, 'unidade', 'armazenamento', 1);
+INSERT INTO `itens` (`id`, `nome`, `quantidade`, `descricao`, `fk_Categoria_id`) VALUES
+(32, 'Lampada 1', 1, 'lampada da sala 1', 2),
+(33, 'notebook 202', 1, 'notebook 2', 1),
+(36, 'béquer', 1, 'asasssasaas', 3),
+(40, 'ssd', 12, 'safsdfsdfs', 1),
+(42, 'alura', 134, 'egfdgfgdfgfd', 2),
+(44, 'uber', 40, 'adm chorão', 2),
+(45, 'agua', 20, 'sgfsdgfsdg', 3),
+(46, 'pedra', 20, '20 kilos de pedra', 3),
+(47, 'clash', 12, 'sfgdgdfgfd', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `perfil`
+-- Table structure for table `perfil`
 --
 
 CREATE TABLE `perfil` (
@@ -91,16 +96,17 @@ CREATE TABLE `perfil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `perfil`
+-- Dumping data for table `perfil`
 --
 
 INSERT INTO `perfil` (`CPF`, `Senha`) VALUES
-('52657628842', 1234);
+('52657628842', 1234),
+('52657628846', 12345);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `saida`
+-- Table structure for table `saida`
 --
 
 CREATE TABLE `saida` (
@@ -111,88 +117,88 @@ CREATE TABLE `saida` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `categoria`
+-- Indexes for table `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Índices para tabela `entrada`
+-- Indexes for table `entrada`
 --
 ALTER TABLE `entrada`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_Itens_id` (`fk_Itens_id`);
 
 --
--- Índices para tabela `itens`
+-- Indexes for table `itens`
 --
 ALTER TABLE `itens`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_Categoria_id` (`fk_Categoria_id`);
 
 --
--- Índices para tabela `perfil`
+-- Indexes for table `perfil`
 --
 ALTER TABLE `perfil`
   ADD PRIMARY KEY (`CPF`);
 
 --
--- Índices para tabela `saida`
+-- Indexes for table `saida`
 --
 ALTER TABLE `saida`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_Itens_id` (`fk_Itens_id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `categoria`
+-- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
   MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de tabela `entrada`
+-- AUTO_INCREMENT for table `entrada`
 --
 ALTER TABLE `entrada`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `itens`
+-- AUTO_INCREMENT for table `itens`
 --
 ALTER TABLE `itens`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
--- AUTO_INCREMENT de tabela `saida`
+-- AUTO_INCREMENT for table `saida`
 --
 ALTER TABLE `saida`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para despejos de tabelas
+-- Constraints for dumped tables
 --
 
 --
--- Limitadores para a tabela `entrada`
+-- Constraints for table `entrada`
 --
 ALTER TABLE `entrada`
   ADD CONSTRAINT `entrada_ibfk_1` FOREIGN KEY (`fk_Itens_id`) REFERENCES `itens` (`id`);
 
 --
--- Limitadores para a tabela `itens`
+-- Constraints for table `itens`
 --
 ALTER TABLE `itens`
   ADD CONSTRAINT `itens_ibfk_1` FOREIGN KEY (`fk_Categoria_id`) REFERENCES `categoria` (`Id`);
 
 --
--- Limitadores para a tabela `saida`
+-- Constraints for table `saida`
 --
 ALTER TABLE `saida`
   ADD CONSTRAINT `saida_ibfk_1` FOREIGN KEY (`fk_Itens_id`) REFERENCES `itens` (`id`);
