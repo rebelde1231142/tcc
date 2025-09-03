68a12a89dbf607afab365c1035e936e012c3863a
Leonel Breno da Silva Ramos de Paula <leonelbrenodasilvagithub@gmail.com>
2025-09-03 11:45:52 -0300

    1111
    1111

--- back/emailService.js
+++ back/emailService.js
@@ -1,22 +1,48 @@
-const nodemailer = require('nodemailer');
 
-// Configure aqui com seu email real e senha de app
-const transporter = nodemailer.createTransport({
-  host: 'smtp.elasticemail.com',
-  port: 2525,
-  auth: {
-    user: 'leonelbrenodasilvagithub1@gmail.com', // seu usuário Elastic Email
-    pass: '9352A5233612E3A7776E90A2563F07E939FE' // sua senha SMTP Elastic Email
-  }
-});
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
+const CLIENT_ID = process.env.GMAIL_CLIENT_ID;
+const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET;
+const REDIRECT_URI = process.env.GMAIL_REDIRECT_URI;
+const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN;
+const EMAIL_USER = process.env.EMAIL_USER;
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 async function enviarEmail(destinatario, assunto, html) {
-  return transporter.sendMail({
-    from: 'leonelbrenodasilvagithub1@gmail.com',
-    to: destinatario,
-    subject: assunto,
-    html
-  });
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: EMAIL_USER,
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken.token
+      }
+    });
+
+    const mailOptions = {
+      from: EMAIL_USER,
+      to: destinatario,
+      subject: assunto,
+      html
+    };
+
+    const result = await transporter.sendMail(mailOptions);
+    return result;
+  } catch (error) {
+    console.error('Erro ao enviar email:', error);
+    throw error;
+  }
 }
 
 module.exports = { enviarEmail };
